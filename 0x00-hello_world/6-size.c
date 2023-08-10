ca666fb13c190dc95a8a763ed8640c09436b6aab
abdofola <abdofola67@gmail.com>
2023-08-10 19:48:20 +0300

    Fix the format

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %c byte(s)", sizeof(char));
+	printf("Size of a char: %d byte(s)", sizeof(char));
 	printf("\nSize of an int: %d byte(s)", sizeof(char));
-	printf("\nSize of a long: %ld byte(s)", sizeof(char));
+	printf("\nSize of a long: %d byte(s)", sizeof(char));
 	printf("\nSize of a long long int: %d byte(s)", sizeof(char));
-	printf("\nSize of a float: %f byte(s)", sizeof(char));
+	printf("\nSize of a float: %d byte(s)", sizeof(char));
 	return (0);
 }
