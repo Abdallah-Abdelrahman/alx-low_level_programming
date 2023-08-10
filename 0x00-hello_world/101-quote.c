7b33f75a3e6853c0b2f4d6015dac2acd81540ed8
abdofola <abdofola67@gmail.com>
2023-08-10 21:59:27 +0300

    Add trailing new line at the end of the string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,9 @@
   */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, str, 59);
+	write(1, str, 60);
 	return (1);
 }
 
