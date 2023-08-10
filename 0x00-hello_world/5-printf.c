6cc3882cbd63f56ef15fa463eb0a590e72589fcd
abdofola <abdofola67@gmail.com>
2023-08-10 19:17:39 +0300

    Print string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+  * main - print to the stdout using printf
+  * Return: 0 as success value status
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
