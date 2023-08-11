a2346814c5df95f8f0db17b3889a39b0c24d8b8b
abdofola <abdofola67@gmail.com>
2023-08-12 01:49:47 +0300

    Print all digits 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - print all digits base 0-9 
+*
+* Return: 0 as exit status
+*/
+int main(void)
+{
+	int count = -1;
+
+	while (++count < 10)
+		printf("%d", count);
+	printf("\n");
+
+	return (0);
+}
