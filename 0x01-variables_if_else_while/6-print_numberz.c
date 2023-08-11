a9abdbd0e42e90cf450e1af626d7b5442608ae70
abdofola <abdofola67@gmail.com>
2023-08-12 01:59:23 +0300

    Print all digits 0-9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Print all digits 0-9 using putchar
+*
+* Return: 0 as exit status
+*/
+int main(void)
+{
+	int count = 47;
+
+	while (++count < 85)
+		putchar(count);
+	putchar(10);
+	return (0);
+}
