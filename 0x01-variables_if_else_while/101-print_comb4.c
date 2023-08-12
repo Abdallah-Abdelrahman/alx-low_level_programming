260d694e19317aa43745560309c4ef0d87650726
abdofola <abdofola67@gmail.com>
2023-08-12 05:55:35 +0300

    Print all possible different combinations of 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+* main -prints all possible different combinations of three digits.
+*
+* Return: 0 as exit status
+*/
+int main(void)
+{
+	int i = 0, start = 48, end = 58, comma = 44, space = 32, j, k, l;
+
+	while (i < 120)
+	{
+		for (j = start; j < end - 1; j++)
+		{
+			for (l = j + 1; l < end - 1; l++)
+			{
+				for (k = l + 1; k < end - 1; k++)
+				{
+					i++;
+					putchar(j);
+					putchar(l);
+					putchar(k);
+					if (i == 120)
+						break;
+					putchar(comma);
+					putchar(space);
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
