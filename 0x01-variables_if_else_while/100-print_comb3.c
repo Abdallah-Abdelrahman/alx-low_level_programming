ca1cbad0375ae0e3f319922251761b73bc555690
abdofola <abdofola67@gmail.com>
2023-08-12 04:54:43 +0300

    Print all possible combinations of 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - print all possible different combinations of 2 digits
+*
+* Return: 0 as exit status
+*/
+int main(void)
+{
+	int count = 47;
+	int comma = 44;
+	int space = 32;
+	int n = 10, i = 0, j, k;
+
+	while (i < 45)
+	{
+		for (j = i; j < n - 1; j++)
+		{
+			for (k = j + 1; k < n; k++)
+			{
+				i++;
+				putchar(j);
+				putchar(k);
+				if (i == 44)
+					break;
+				putchar(comma);
+				putchar(space);
+			}
+		}
+	}
+
+	return (0);
+}
