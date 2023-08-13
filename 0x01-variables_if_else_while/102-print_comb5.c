3d3cc069f55fd11cc482f0ebee74ff014858ad4c
abdofola <abdofola67@gmail.com>
2023-08-14 01:55:51 +0300

    Print all possible combinations of two two-digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,5 +7,30 @@
 */
 int main(void)
 {
+	int i = 0, k = i + 1,
+	    comma = 44, space = 32, chars[] = {48, 49, 50, 51, 52, 53, 54, 56, 57};
+
+	while (i < 99)
+	{
+		while (j < 100)
+		{
+			putchar(chars[i / 10]);
+			putchar(chars[i % 10]);
+			putchar(space);
+			putchar(chars[j / 10]);
+			putchar(chars[j % 10]);
+			if (i != 98)
+			{
+				putchar(comma);
+				putchar(space);
+			}
+			j++;
+		}
+		/* book keeping 0_o */
+		i++;
+		j += i;
+	}
+	putchar(10);
+
 	return (0);
 }
