f3adc497d9b9ac5edcce369314545008211fe3c9
abdofola <abdofola67@gmail.com>
2023-08-14 02:06:50 +0300

    Rename variable

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,17 +8,17 @@
 int main(void)
 {
 	int i = 0, j = i + 1,
-	    comma = 44, space = 32, chars[] = {48, 49, 50, 51, 52, 53, 54, 56, 57};
+	    comma = 44, space = 32, nums[] = {48, 49, 50, 51, 52, 53, 54, 56, 57};
 
 	while (i < 99)
 	{
 		while (j < 100)
 		{
-			putchar(chars[i / 10]);
-			putchar(chars[i % 10]);
+			putchar(nums[i / 10]);
+			putchar(nums[i % 10]);
 			putchar(space);
-			putchar(chars[j / 10]);
-			putchar(chars[j % 10]);
+			putchar(nums[j / 10]);
+			putchar(nums[j % 10]);
 			if (i != 98)
 			{
 				putchar(comma);
