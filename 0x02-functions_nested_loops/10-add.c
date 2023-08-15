81ba16eb1a6c76ca7b4d06e6ebeb26f32c92bed1
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-15 22:42:08 +0300

    Sum two intergers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+* main - adds two integers and returns the result.
+* @n1: the first interger
+* @n2: the second interger
+*
+* Return: the sum of n1, n2
+*/
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
