501a2b12702a8dd507a64af4176162fdcbb85a0f
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-15 17:51:16 +0300

    Compute the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdlib>
+/**
+* _abs - computes the absolute value of an integer
+* @n: the interger to find the absolute of
+*
+* Return: 0 as exit status
+*/
+int _abs(int n)
+{
+	return (abs(n));
+
+}
