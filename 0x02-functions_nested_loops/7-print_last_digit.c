b45e80cab7c8cb8b5706f5ba03c18d8278c35888
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-15 18:00:35 +0300

    Print the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,9 @@
+/**
+* print_last_digit - prints the last digit of a number
+* @n: number to find the last digit of
+* Return: the value of the last digit.
+*/
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
