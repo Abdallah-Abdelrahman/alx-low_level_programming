04e6116410504706923a19c4adea01d1f196b3d7
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-15 10:51:45 +0300

    Print _putchar to stdout using _putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "_putchar.h"
+
+/**
+* main - write your short description
+* Description: Long desc
+*
+* Return: 0 as exit status(SUCCESS)
+*/
+int main(void)
+{
+	_putchar(1, "_putchar\n", 9);
+	return (0);
+}
