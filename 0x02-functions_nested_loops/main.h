3b0993466f5fd009b7e61bface30cf27aa8ddb82
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-16 04:35:56 +0300

    Test

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#ifndef HEADER
-#define HEADER
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -16,38 +14,3 @@ void print_precision(int);
 void print_times_table(int n);
 void print_table(int);
 void print_2p(int);
-/**
-  * print_precision - print interger based on precision
-  * @n: number to print.
-  */
-void print_precision(int n)
-{
-	int f, s, t;
-
-	if (n > 99)
-	{
-		f = n % 10;
-		s = (n / 10) % 10;
-		t = (n / 10) / 10;
-		_putchar(t + '0');
-		_putchar(s + '0');
-		_putchar(f + '0');
-
-	}
-
-	if (n < 100 && n > 9)
-	{
-
-		f = n % 10;
-		s = n / 10;
-		_putchar(s + '0');
-		_putchar(f + '0');
-
-	}
-
-	if (n < 10)
-	{
-		_putchar(n + '0');
-	}
-}
-#endif
