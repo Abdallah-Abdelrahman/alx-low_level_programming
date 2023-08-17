4dc82c0db726186df7330b8c902695e75f579bf7
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-17 13:46:28 +0300

    Make a separate function for each year type

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,28 +13,52 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && (year % 100 == 0))
-	{
-		if (month >= 2 && day >= 60)
-		{
-			day++;
-		}
 
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
-	}
+	if (year % 4 != 0)
+		print_simple(month, day, year);
+
+	else if (year % 100 != 0)
+		print_leap(month, day, year);
+
+	else if (year % 400 != 0)
+		print_simple(month, day, year);
 
 	else
-	{
+		print_leap(month, day, year);
+}
 
-		if (month == 2 && day >= 60)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
+/**
+  * print_simple - print simple year
+  * @m: month
+  * @d: day
+  * @y: year
+  */
+void print_simple(int m, int d, int y)
+{
+	if (m == 2 && d == 60)
+	{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 	}
+	else
+	{
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 365 - day);
+	}
+
+}
+
+/**
+  * print_leap - print simple year
+  * @m: month
+  * @d: day
+  * @y: year
+  */
+void print_leap(int m, int d, int y)
+{
+	if (m >= 2 && d >= 60)
+		d++;
+
+	printf("Day of the year: %d\n", day);
+	printf("Remaining days: %d\n", 366 - day);
+
 }
