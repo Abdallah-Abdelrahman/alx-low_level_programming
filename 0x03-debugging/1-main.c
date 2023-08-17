53be04ef59302cc9410440d50474b8216f28fa6b
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-17 09:55:17 +0300

    Update the header

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
 * main - causes an infinite loop
@@ -13,9 +13,7 @@ int main(void)
 
 	i = 0;
 
-	/**
-	  * while (i < 10)
-	 */
+	/* while (i < 10) */
 	{
 		putchar(i);
 	}
