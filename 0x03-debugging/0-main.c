2291b6dc1f35c678506a2ed58cf91e7266a2b819
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-17 09:29:42 +0300

    Change variable name to i

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,15 +2,15 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-*
-* Return: 0 as exit status
+* Return: 0
 */
+
 int main(void)
 {
-	int n;
+	int i;
 
-	n = 0;
-	positive_or_negative(n);
+	i = 0;
+	positive_or_negative(i);
 
 	return (0);
 }
