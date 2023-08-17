8a22267c2cffaa16e4dd18146b8e1a0060e8d3bc
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-17 09:18:03 +0300

    Test positive_or_negative function in case of 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,10 @@
+/**
+* main - test that the function positive_or_negative() gives the correct output
+*
+* Return: 0 as exit status
+*/
+int main(void)
+{
+	positive_or_negative(0);
+	return (0);
+}
