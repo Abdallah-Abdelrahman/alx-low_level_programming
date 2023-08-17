85ed458e87c74c65f8e3514cb376d0ddc50e324e
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-17 09:25:30 +0300

    Add space after the hyphen in main docs

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
-* main -tests function that prints if integer is positive or negative
+* main - tests function that prints if integer is positive or negative
 *
 * Return: 0 as exit status
 */
 int main(void)
 {
 	positive_or_negative(0);
+
 	return (0);
 }
