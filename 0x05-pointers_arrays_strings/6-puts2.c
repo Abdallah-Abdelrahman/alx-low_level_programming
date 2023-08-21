16941a0fcf609f6c23ba379cf084dd3dcb273e79
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-22 01:27:51 +0300

    Account for the second dot

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,15 @@
  */
 void puts2(char *str)
 {
+	int i = 0;
+
 	while (*str)
 	{
-		_putchar(*str);
 		if (*str == '.')
-			break;
+			i++;
+
+		if (i < 3)
+			_putchar(*str);
 		str += 2;
 	}
 	_putchar('\n');
