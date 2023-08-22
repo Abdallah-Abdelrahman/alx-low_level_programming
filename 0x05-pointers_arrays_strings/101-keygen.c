4edb9f53662d0f1dca9fd3f45f0eecb423458ac4
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-22 05:19:04 +0300

    Generate random valid password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - generates random valid passwords for the program 101-crackme.
+ * @argc: number of arguments
+ * @argv: string pointer
+ *
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	int r, nloops;
+	unsigned int seed;
+
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage: %s <seed> <nloops>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	seed = atoi(argv[1]);
+	nloops = atoi(argv[2]);
+
+	srand(seed);
+	for (int j = 0; j < nloops; j++)
+	{
+		r =  rand();
+		printf("%d\n", r);
+	}
+
+	return (exit(EXIT_SUCCESS));
+}
