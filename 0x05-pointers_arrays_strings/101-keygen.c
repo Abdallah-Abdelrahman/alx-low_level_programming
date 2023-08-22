07807f35625e1fff7fc3d2572e44bbc1c68990f9
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-22 11:18:32 +0300

    Try 001

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,17 +14,18 @@ int main(int argc, char *argv[])
 	int j, r, nloops;
 	unsigned int seed;
 
-	if (argc != 2)
+	if (argc < 2)
 		fprintf(stderr, "Usage: %s <seed> <nloops>\n", argv[0]);
 
-	seed = atoi(argv[1]);
-	nloops = atoi(argv[2]);
+	seed = atoi(argv[0]);
+	nloops = atoi(argv[1]);
 
 	srand(seed);
 	for (j = 0; j < nloops; j++)
 	{
 		r =  rand();
 		printf("%d\n", r);
+		printf("%d: %s", j, argv[j]);
 	}
 
 	return (0);
