67a80d3d9a41168d72ca152e0d9f681ec18483ba
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-24 00:20:37 +0300

    Concatenate using n bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * _strncat - concatenates 2 strings using n bytes,
+ * from source string.
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes from src
+ *
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	char *ptr = dest;
+
+	for (; *dest; dest++)
+		;
+	for (; i < n && src[i]; src++, dest++, i++)
+		*dest = *src;
+	*dest = '\0';
+
+	return (ptr);
+}
