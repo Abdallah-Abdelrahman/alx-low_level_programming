74fcca5a5bf261b44159e83eee853200e90bb686
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-23 07:32:52 +0300

    Refactor

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,21 +9,16 @@ void puts_half(char *str)
 {
 	int len = 0, count = 0;
 
-	while (*str)
-	{
-		str++;
-		len++;
-	}
+	for (; *str; str++, len++)
+		;
+
 	if (len % 2 == 0)
 		count = len / 2;
 	else
 		count = (len - 1) / 2;
 
-	while (count > 0)
-	{
+	for (; *(str - count); count--)
 		_putchar(*(str - count));
-		count--;
-	}
-	_putchar('\n');
 
+	_putchar('\n');
 }
