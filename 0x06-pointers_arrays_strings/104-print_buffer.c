532832b10e664dd8f79d978cad04eb346834f2ab
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-25 05:54:29 +0300

    Print a buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_buffer - write your short description
+ * @b: pointer to string
+ * @size: size
+ *
+ */
+void print_buffer(char *b, int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+}
