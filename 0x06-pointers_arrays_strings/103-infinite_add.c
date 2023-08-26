e31d8e99c5e269b4fdbc25d99bbb340abd13cb61
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-26 05:30:14 +0300

    Add 2 numbers -111-

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -18,7 +18,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		;
 	for (; n2[len_n2]; len_n2++)
 		;
-
 	/* length of either is greater than buffer size */
 	if (len_n1 + 1 >= size_r || len_n2 + 1 >= size_r)
 		return (0);
@@ -29,10 +28,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		int valid_n1 = len_n1 - 1 >= 0 ? n1[len_n1 - 1] : '0';
 		int valid_n2 = len_n2 - 1 >= 0 ? n2[len_n2 - 1] : '0';
 
+		if (len_n1 < 0 && len_n2 < 0)
+			break;
 		sum = (valid_n1 - '0') + (valid_n2 - '0') + carry;
 		r[len] = (sum % 10) + '0';
 		carry = sum / 10;
-		printf("len = %d\t r = %s\n", len, r);
 	}
 
 	rev_string(r, len);
