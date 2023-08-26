1a1dda700d4a0c656eed941a16f4f153c9bcc6d5
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-26 05:52:21 +0300

    Remove comments

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * infinite_add - adds 2 numbers
@@ -18,10 +17,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		;
 	for (; n2[len_n2]; len_n2++)
 		;
-	/* length of either is greater than buffer size */
 	if (len_n1 + 1 >= size_r || len_n2 + 1 >= size_r)
 		return (0);
-	/* calculate the sum */
 	for (; len < size_r - 1; len_n1--, len_n2--, len++)
 	{
 		int valid_n1 = len_n1 - 1 >= 0 ? n1[len_n1 - 1] : '0';
@@ -29,12 +26,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 		if (len_n1 < 0 && len_n2 < 0)
 			break;
+
 		sum = (valid_n1 - '0') + (valid_n2 - '0') + carry;
 		r[len] = (sum % 10) + '0';
 		carry = sum / 10;
 	}
-	if (len > size_r)
-		return (0);
 	rev_string(r, len);
 	return (r);
 }
@@ -54,7 +50,6 @@ void rev_string(char *str, int size)
 		*(str + i) = *(str + (size - 1));
 		*(str + (size - 1)) = tmp;
 
-		/* book keeping o_0 */
 		i++;
 		size--;
 	}
