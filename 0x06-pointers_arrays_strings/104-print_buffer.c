be0bde5072cbfc7faf8b8127482198fd284cf1b1
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-27 02:08:21 +0300

    Print the hex positions & buffer content

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -7,6 +7,25 @@
  */
 void print_buffer(char *b, int size)
 {
+	int i = 0, j, k = 0;
+
 	if (size <= 0)
-		_putchar('\n');
+		printf("\n");
+
+	for (; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+
+		for (j = 0; j < 5; j++)
+		{
+			char h_00 = b[(j + k) * 2];
+			char h_01 = b[((j + k) * 2) + 1];
+			printf("%02x%02x ", h_00, h_01);
+		}
+
+		printf("\n");
+		k += 5;
+	}
+
+
 }
