8f923307cc2d4f3b3d2d3aa35e6c1e4db877db51
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-27 05:06:24 +0300

    Print 10 bytes of buffer in each line

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -7,12 +7,12 @@
  */
 void print_buffer(char *b, int size)
 {
-	int i = 0, j, k = 0;
+	int i = 0, k = 0, j, l;
 
 	if (size <= 0)
 		printf("\n");
 
-	for (; i < size; i += 10)
+	for (; i < size; i += 10, k += 5)
 	{
 		printf("%08x: ", i);
 
@@ -20,12 +20,25 @@ void print_buffer(char *b, int size)
 		{
 			char h_00 = b[(j + k) * 2];
 			char h_01 = b[((j + k) * 2) + 1];
-			printf("%02x%02x ", h_00, h_01);
+			if ((j + k) * 2 < size)
+				printf("%02x%02x ", h_00, h_01);
+		}
+
+		/* print 10 bytes of buffer */
+		for (l = 0; l < 10; l++)
+		{
+			switch (b[l + i])
+			{
+				case '\0': case '\1': case '\2': case '\3':
+				case '\4': case '\5': case '\6': case '\7':
+				case '\n':
+					printf(".");
+					break;
+				default:
+					printf("%c", b[l + i]);
+			}
 		}
 
 		printf("\n");
-		k += 5;
 	}
-
-
 }
