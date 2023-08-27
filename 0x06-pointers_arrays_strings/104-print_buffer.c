8709904ddb6e5ddc23e3f2a3226699188f1be270
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-27 17:32:55 +0300

    Use isprint funciton

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <ctype.h>
 
 /**
  * print_buffer - prints buffer
@@ -41,24 +42,14 @@ void printable_10b(int index, char *buff, int size)
 {
 	int i;
 
-	/**
-	 * the problem is, how to get it from character to decimal (00)
-	 * A: if it's divisble in the first place, than it's character (solved)
-	 * after you get it, compare it against this range (0 - 31) (01)
-	 * on success print dot. (02)
-	 * on failure print the character (03)
-	 */
 	for (i = 0; i < 10; i++)
 	{
 		if (i + index + 1 > size)
 			break;
-		if (buff[i + index] / 10 && buff[i + index] != '\n')
-		{
+
+		if (isprint(buff[i + index]) && buff[i + index] != '\n')
 			printf("%c", buff[i + index]);
-		}
 		else
-		{
 			printf(".");
-		}
 	}
 }
