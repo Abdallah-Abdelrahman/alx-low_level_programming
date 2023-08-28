c224eca71c18a768a4d8e9d0e60d51b84ea5dc57
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-28 03:57:29 +0300

    Adhere to 40 line per function

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,12 +11,11 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int carry = 0, len_n1 = 0, len_n2 = 0, idx = 0, sum = 0, largest = 0;
+	int carry = 0, len_n1 = 0, len_n2 = 0,
+	    idx = 0, sum = 0, largest = 0, arr[2] = {0, 0},
+	    *lens = len2str(n1, n2, arr);
+	len_n1 = lens[0], len_n2 = lens[1], largest = len_n1;
 
-	for (; n1[len_n1]; len_n1++, largest++)
-		;
-	for (; n2[len_n2]; len_n2++)
-		;
 	if (len_n2 > len_n1)
 		largest = len_n2;
 	if (largest == size_r)
@@ -58,4 +57,26 @@ void rev_string(char *str, int size)
 		i++;
 		size--;
 	}
+}
+
+/**
+ * len2str - calculate the lenght of 2 strings
+ * @p1: 1st string
+ * @p2: 2nd string
+ * @arr: array of 2 elems represenet the initial
+ * size of each
+ *
+ * Return: pointer to array of 2 integer
+ */
+int *len2str(char *p1, char *p2, int arr[])
+{
+	for (; p1[arr[0]] || p2[arr[1]]; )
+	{
+		if (p1[arr[0]])
+			arr[0]++;
+		if (p2[arr[1]])
+			arr[1]++;
+	}
+
+	return (arr);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,6 +14,7 @@ char *leet(char *);
 char *rot13(char *);
 void print_chars(unsigned int n);
 void print_number(int n);
+int *len2str(char *, char *, int *);
 void rev_string(char *str, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void printable_10b(int index, char *buff, int size);
