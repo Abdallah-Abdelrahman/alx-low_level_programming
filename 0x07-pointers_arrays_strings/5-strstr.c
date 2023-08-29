52016a97908b98c8e71a387826da00b52a58e0d0
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-29 17:32:56 +0300

    Cover the case of empty string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,6 +10,31 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	return (strstr(haystack, needle));
+	int i = 0, j = 0, len = strlen(needle);
+	char *ptr = NULL;
 
+	if (len == 0)
+		return (haystack);
+	for (; haystack[i]; i++)
+	{
+		for (; needle[j]; )
+		{
+			if (haystack[i] != needle[j])
+			{
+				j = 0;
+				ptr = NULL;
+				break;
+			}
+			if (haystack[i] == needle[j])
+			{
+				if (j == 0)
+					ptr = &haystack[i];
+				j++;
+				break;
+			}
+		}
+	}
+	if (j != len)
+		ptr = NULL;
+	return (ptr);
 }
