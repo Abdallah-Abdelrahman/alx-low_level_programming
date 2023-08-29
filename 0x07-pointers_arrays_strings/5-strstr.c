083c84ec768ca23b063f964864142613e0a32dd9
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-29 05:41:41 +0300

    Use standard library

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,29 +10,6 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0, j = 0, len = strlen(needle);
-	char *ptr;
-
-	for (; haystack[i]; i++)
-	{
-		for (; needle[j]; )
-		{
-			if (haystack[i] != needle[j])
-			{
-				j = 0;
-				break;
-			}
-			if (haystack[i] == needle[j])
-			{
-				if (j == 0)
-					ptr = &haystack[i];
-				j++;
-				break;
-			}
-		}
-	}
-	if (j != len)
-		return (NULL);
-	return (ptr);
+	return (strstr(haystack, needle));
 
 }
