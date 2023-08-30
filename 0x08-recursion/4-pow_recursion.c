d3108722f930b002ee02466b481c6180a0cf5038
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-30 18:24:19 +0300

    Calculate `x^y`

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculate the power of x^y
+ * @x: the base
+ * @y: the power
+ *
+ * Return: x^y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
+}
