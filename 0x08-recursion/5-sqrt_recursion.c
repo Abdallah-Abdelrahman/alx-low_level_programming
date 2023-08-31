ca808c15d3a8c118aedce09a512cbc149b4b8eed
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-08-31 05:31:25 +0300

    Calculate the square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,5 +12,30 @@ int _sqrt_recursion(int n)
 		return (-1);
 	if (n <= 1)
 		return (n);
-	return (n);
+	return (diffing(n, 0, n));
+}
+
+/**
+ * diffing - get the square root by diffing last from first
+ * @n: integer number
+ * @f: first
+ * @l: last
+ * 
+ * Return: square root
+ */
+int diffing(int n, int f, int l)
+{
+	int m, sqrt_m;
+
+	if (f > l)
+		return (l);
+
+	m = f + (l - f) / 2;
+	sqrt_m = m * m;
+	if (sqrt_m == n)
+		return (sqrt_m);
+	if (sqrt_m < n)
+		return (diffing(n, m + 1, n));
+	else
+		return (diffing(n, f, m - 1));
 }
