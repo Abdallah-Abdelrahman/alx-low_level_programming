67eb021e2f6d78d85b55fd80b26305f4c0df7567
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-02 02:02:45 +0300

    Fix the problem with conflict _putchar

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,19 +1,12 @@
-#include "main.h"
+#include <unistd.h>
 /**
-* main - Print _putchar to stdout using _putchar.
+* _putchar - write the character c to stdout
+* @c: The character to print
 *
-* Return: 0 as exit status(SUCCESS)
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
 */
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
