a9f16f0503665ecf76b372afc45c27a7c7f6ec34
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-02 03:09:22 +0300

    Program prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * whatsmyname - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of pointers to the strings
+ *
+ * Return: 0 as exit status
+ */
+int main(int UNUSED(argc), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
