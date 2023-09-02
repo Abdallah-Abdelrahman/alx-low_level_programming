706fc94a54837222aa8e3200d180cf49047f6fd9
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-02 05:24:10 +0300

    Filter condition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints number of arguments passed to it
+ * @argc: number of args
+ * @argv: array of pointers to string
+ *
+ * Return: 0 as exit status
+ */
+int main(int argc, char **argv)
+{
+	if (!*arv[1])
+		printf("%d\n", 0);
+
+	return (0);
+}
