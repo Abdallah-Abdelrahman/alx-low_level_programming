7ae99bbf4ca03ab6710ca4f2c6fbdc761a6ec274
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-04 02:10:31 +0300

    Remove duplicate definition of putchar

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -46,15 +46,3 @@ void print_chars(unsigned long int n)
 	else
 		_putchar(n + '0');
 }
-
-/**
-* _putchar - write the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
