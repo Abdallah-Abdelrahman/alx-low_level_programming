57f3af713233b704a82acc0b5c26d73f21caaf54
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-05 11:04:41 +0300

    Create grid initialized with zero

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: columns
+ * @height: rows
+ *
+ * Return: pointer to 2 dimensional array,
+ * NULL on failure
+ */
+int **alloc_grid(int width, int height)
+{
+	int *grid[], i = 0, j;
+
+	if ((width || height) <= 0)
+		return (0);
+
+	grid = malloc(sizeof(*grid) * width * height);
+	if (!grid)
+		return (0);
+
+	for (; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+
+	return (grid);
+}
