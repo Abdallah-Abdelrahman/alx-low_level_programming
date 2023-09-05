e3ad83086148fd78ed1b9f11064c47fcee9d57e6
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-05 15:15:07 +0300

    Initial commit

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: number of arguments
+ * @av: array of pointers to string
+ *
+ * Return: pointe to the new string,
+ * NULL if it fails.
+ */
+char *argstostr(int ac, char **av)
+{
+	if (arg < 2)
+		return (0);
+	if (!*av)
+		return (0);
+}
