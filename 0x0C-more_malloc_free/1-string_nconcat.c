b07aebfb74bd03b325159f1eb61021f88270e754
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-08 01:15:15 +0300

    string_nconcat 011

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,7 +21,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			len2++;
 	}
 	size = len1 + (n >= len2 ? len2 : n);
-	ptr = malloc(sizeof(*ptr) * size + 1);
+	ptr = malloc((sizeof(*ptr) * size) + 1);
 
 	if (!ptr)
 	{
