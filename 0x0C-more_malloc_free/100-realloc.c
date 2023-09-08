6856171bda31f2b81691f5ca919acaf95eafa0ed
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-08 12:01:33 +0300

    Mock realloc 003

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,10 +13,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	if (new_size == 0 && ptr)
 		return (0);
-	if (new_size == 0 && old_size == 0)
-		return (0);
 	if (new_size == old_size)
 		return (ptr);
+	if (new_size == 0 && old_size == 0)
+		return (0);
 	if (!ptr)
 		return (_malloc(new_size));
 
