01fe9e622c0a27f5479a8eb51bd2d9b0c90fb107
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-12 04:53:39 +0300

    Initialize dog 004

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -23,22 +23,15 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	(*d).name = malloc(len1 + 1);
 	(*d).owner = malloc(len2 + 1);
 
-	if (!d->name || !d->owner)
+	for (len1 = 0; name[len1]; len1++)
 	{
-		free(d->name);
-		free(d->owner);
-		exit(98);
-	}
-
-	for (len1 = 0, len2 = 0; name[len1] || owner[len2]; )
-	{
-		if (name[len1])
-			(*d).name[len1] = name[len1];
-		if (owner[len2])
-			(*d).owner[len2] = owner[len2];
-		len1++, len2++;
+		(*d).name[len1] = name[len1];
 	}
 
 	(*d).name[len1] = 0;
+	for (len2 = 0; name[len2]; len2++)
+	{
+		(*d).name[len2] = name[len2];
+	}
 	(*d).owner[len2] = 0;
 }
