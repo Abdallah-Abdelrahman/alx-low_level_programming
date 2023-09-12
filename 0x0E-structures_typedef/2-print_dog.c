207de33ed1168c1b4f0b28b9a6e3657b28549b8d
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-12 05:15:54 +0300

    Print dog 000

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,7 @@ void print_dog(struct dog *d)
 {
 	if (!d)
 		return;
-	printf("Name: %s\nAge: %f\n,Owner: %s\n",
+	printf("Name: %s\nAge: %f\nOwner: %s\n",
 			d->name ? d->name : "(nil)",
 			d->age, d->owner ? d->owner : "(nil)");
 }
--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -13,9 +13,10 @@ int main(int ac, char **av)
 	(void)ac;
 	(void)av;
 
-	init_dog(&my_dog, "Poppy", 3.5, "Bob");
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-	free(my_dog.name);
-	free(my_dog.owner);
+
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	print_dog(&my_dog);
 	return (0);
 }
