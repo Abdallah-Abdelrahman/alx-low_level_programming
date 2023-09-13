6a85a6f48fd2eb13ec136ddb84acfb574dea081b
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-13 19:39:41 +0300

    Print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - write your short description
+ * @name: string pointer
+ * @f: pointer to a func, that takes string pointer
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
