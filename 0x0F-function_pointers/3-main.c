024b8ff62579015c4e4fb51940c794f7eecafe9c
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-14 00:16:20 +0300

    Simple operations 005

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -31,28 +31,3 @@ int main(int ac, char **av)
 
 	return (0);
 }
-
-#if 0
-/**
- * print_chars - prints long integer number recursively
- * @n: number
- */
-void print_chars(int n)
-{
-	if (n > 9)
-		print_chars(n / 10);
-	_putchar((n % 10) + '0');
-}
-
-/**
- * print_error - prints Error
- * @str: Error string
- */
-void print_error(char *str)
-{
-	int i;
-
-	for (; str[i]; i++)
-		_putchar(str[i]);
-}
-#endif
