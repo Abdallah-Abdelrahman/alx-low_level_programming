b614f39673de0130469e4c8faf030e94494cd140
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-22 02:31:09 +0300

    Print all element of signly linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - write your short description
+ * @h: pointer to singly linked list
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t len = 0, i;
+	char *nil = "[0] (nil)";
+
+	for (; h && h->next; len++, h = h->next)
+	{
+		if (!h->str)
+			printf("%s\n", nil);
+		else
+		{
+			for (i = 0; h->str[i]; i++)
+				;
+			printf("[%lu] %s\n", i, h->str);
+		}
+	}
+
+	return (len);
+}
