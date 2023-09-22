1af63be2e3a41b50a4b5b1bf006f7d4f3cc204d5
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-22 04:25:57 +0300

    Handle when the node is first to add

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <string.h>
 
 /**
  * add_node_end - adds a new node at the end of a list_t list.
@@ -11,20 +10,48 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *tmp = *head, *tail = malloc(sizeof(*tail));
+	list_t *tmp = *head, *tail;
 
+	if (!tmp)
+	{
+		return (add_node(head, str));
+	}
+	tail = malloc(sizeof(*tail));
 	if (!tail)
 		return (NULL);
 
 	for (; tmp; tmp = tmp->next)
 	{
-		if (!tmp->next)
+		if (!(tmp->next))
 		{
 			tmp->next = tail;
-			tail->next = NULL;
 			tail->str = strdup(str);
+			tail->next = NULL;
 		}
 	}
 
-	return (0);
+	return (tail);
+}
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: the head of the list
+ * @str: string pointer as member of the node
+ *
+ * Return: address to the newly created node,
+ * `NULL` on failure
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->next = *head;
+	*head = new;
+	new->str = strdup(str);
+
+	return (new);
 }
