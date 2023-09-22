6f6f0b6e82af3742665253e26e5c2981bc569e50
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-22 03:44:56 +0300

    Return NULL on failure

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,11 +13,12 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
 
 	new->next = *head;
 	*head = new;
-	if (str)
-		new->str = strdup(str);
+	new->str = strdup(str);
 
 	return (new);
 }
