4499311fe6304b641f8784658194e52f6bdb7c26
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-25 21:25:31 +0300

    Bettify

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -52,28 +52,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int cursor = 0;
 
 	if (!head)
 		return (0);
-
-	return (search_node(head->next, index, cursor));
-}
-
-/**
- * search_node - search node at particular index.
- * @head: head pointer
- * @idx: index fo targeted node
- * @cursor: index of node at any given moment
- *
- * Return: returns the nth node,
- * `NULL` if it does not exist
- */
-listint_t *search_node(listint_t *head, unsigned int idx, unsigned int cursor)
-{
-	if (!head)
-		return (0);
-	if (cursor == idx)
+	if (index == 0)
 		return (head);
-	return (search_node(head->next, idx, cursor + 1));
+
+	return (get_nodeint_at_index(head->next, index - 1));
 }
+
