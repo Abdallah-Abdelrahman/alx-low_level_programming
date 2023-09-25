1f2ea243300a258283e24d6af51699061f179e09
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-25 23:53:33 +0300

    Insert node 002

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *node;
 
-	if (idx > _list_len(*head) && head)
+	if (idx > _list_len(*head) && !head)
 		return (0);
 	if (!*head)
 	{
