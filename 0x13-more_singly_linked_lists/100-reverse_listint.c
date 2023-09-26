94861e655764569a7ee2af5c04922ba77808485b
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-26 20:58:31 +0300

    Remove `tmp`

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -19,25 +19,22 @@ listint_t *reverse_listint(listint_t **head)
  * reverse - reverse singly linked list
  * @head: address of the head pointer
  * @len: size of the list
- * @idx: current index of the node
+ * @idx: current index of the list start at `0`
  *
  * Return: pointer to the 1st node of the reversed list
  */
 listint_t *reverse(listint_t **head, unsigned int len, unsigned int idx)
 {
-	int tmp;
-
-	if (len == 1)
+	if (len == 1 || len - 1 == idx)
 		return (*head);
 	if (idx < len)
 	{
-		tmp = get_nodeint_at_index(*head, idx)->n;
-		get_nodeint_at_index(*head, idx)->n
-			= get_nodeint_at_index(*head, len - 1)->n;
-		get_nodeint_at_index(*head, len - 1)->n = tmp;
+		(*head)->n ^= get_nodeint_at_index(*head, len - idx - 1)->n;
+		get_nodeint_at_index(*head, len - idx - 1)->n ^= (*head)->n;
+		(*head)->n ^= get_nodeint_at_index(*head, len - idx - 1)->n;
 	}
 
-	return (reverse(head, len - 1, idx + 1));
+	return (reverse(&(*head)->next, len - 1, idx + 1));
 }
 
 /**
