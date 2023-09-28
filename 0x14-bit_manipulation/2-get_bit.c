aeb4d521f2eb980f58ebd73ffa01aaacb7da91d2
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-29 02:29:37 +0300

    Git bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_bit - return bit at a given index
+ * @n: number
+ * @index: start form 0
+ *
+ * Return: bit at index,
+ * -1 on error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index < 0)
+		return (-1);
+	return ((n >> index) & 1);
+}
