0111ea3ae90c0f3eb34f578c4fb9d96a61a97e34
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-29 03:22:21 +0300

    Get bit 005

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,9 +10,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-#if 0
-	if ((int)n < 0)
+	if (sizeof(unsigned int) * 8 >= index)
 		return (-1);
-#endif
 	return ((n >> index) & 1);
 }
