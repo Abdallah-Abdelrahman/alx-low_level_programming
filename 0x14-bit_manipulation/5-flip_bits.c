6d5f9df411dff476678c76f278bc1aff00080273
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-29 04:15:06 +0300

    Count the number of bits to flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * flip_bits - write your short description
+ * @n: 1st number
+ * @m: 2nd number
+ *
+ * Return: the number of bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	n ^= m;
+
+	return (count_set(n));
+}
+
+/**
+ * count_set - count the 1's
+ * @n: number
+ *
+ * Return: count of sets
+ */
+unsigned int count_set(unsigned int n)
+{
+	if (!n)
+		return (0);
+	return ((n & 1) + count_set(n >> 1));
+}
