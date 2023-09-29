7e3de9cbd56b4117f2aaba3ffcd700fde6a7a18a
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-29 03:34:56 +0300

    Get bit 006

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * clear_bit - write your short description
+ * Description: Long desc
+ *
+ * Return: 0 as exit status
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	return (0);
+}
