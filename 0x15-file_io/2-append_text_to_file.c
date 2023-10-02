41c045c8fa3049c6be8638ce12d8024a688469ba
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-10-03 00:14:21 +0300

    Append 000

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,12 +10,13 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int i, fd, count, mode = 00600;
+	(void)mode;
 
 	if (!*filename)
 		return (-1);
 	if (!text_content)
 		return (1);
-	fd = open(filename, O_APPEND, mode);
+	fd = open(filename, O_APPEND);
 	if (fd < 0)
 		return (-1);
 	for (i = 0; text_content[i]; i++)
