2392ea4f82da475fd9c9d038876f8fbf7ccf78c4
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-10-02 23:27:45 +0300

    Create file 000

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,5 +10,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	return (0);
+	int fd, count, i, mode = 0;
+
+	if (!*filename)
+		return (-1);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		mode = 0600;
+		close(fd);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (fd < 0)
+		return (-1);
+	for (i = 0; text_content && text_content[i]; i++)
+		;
+	count = write(fd, text_content, i);
+	close(fd);
+
+	return (count < 0 ? -1 : 1);
 }
+
