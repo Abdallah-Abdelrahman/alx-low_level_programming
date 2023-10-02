305fd9a2c56601c7e632380b0b05616d8f28e875
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-10-02 21:47:33 +0300

    Read text file and print it to stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file.
+ * Description: and prints it to the POSIX standard output.
+ * @filename: file name
+ * @letters: number of letters
+ *
+ * Return: the actual number of letters it could read and print,
+ * 0 if file cann't be open or read.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	size_t count, bytes;
+	char *buf = 0;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	buf = malloc(sizeof(*buf) * letters);
+	if (!buf)
+		return (0);
+	bytes = read(fd, buf, letters);
+	if (bytes < 0)
+		return (0);
+
+	count = write(STDOUT_FILENO, buf, letters);
+
+	return (count != letters ? 0 : count);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER
+#define HEADER
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+/* your protos and preprocessors goes here*/
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+#endif /* HEADER */
