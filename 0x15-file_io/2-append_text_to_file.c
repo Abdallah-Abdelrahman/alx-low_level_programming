4ccdb8071de5c9f5681eb2da62b110cc4e2a0089
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-10-03 00:12:10 +0300

    Append to a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: string pointer
+ * @text_content: string pointer
+ * Return: 1 on success,
+ * -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int i, fd, count, mode = 00600;
+
+	if (!*filename)
+		return (-1);
+	if (!text_content)
+		return (1);
+	fd = open(filename, O_APPEND, mode);
+	if (fd < 0)
+		return (-1);
+	for (i = 0; text_content[i]; i++)
+		;
+	count = write(fd, text_content, i);
+
+	return (count < 0 ? -1 : 1);
+}
