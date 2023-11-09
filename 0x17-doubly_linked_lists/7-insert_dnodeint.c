1dd9d8c8977433cac40ec95ef31f0d9d646585a8
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-09 17:39:06 +0300

    Insert at 013

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int len = dlistint_len(*h);
 
-	if (idx > len)
+	if (idx >= len)
 		return (0);
 	return (insert(h, idx, n));
 }
