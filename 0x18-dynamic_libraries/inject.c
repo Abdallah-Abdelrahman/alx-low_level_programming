9c7e147f4ba551fd6ef791b9bb0b32ec2f9fd57d
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-15 01:09:10 +0300

    Jackpot 005

--- 0x18-dynamic_libraries/inject.c
+++ 0x18-dynamic_libraries/inject.c
@@ -1,37 +1,19 @@
 #include <stdio.h>
 
 /**
- * inject - write your short description
+ * rand - write your short description
  *
  * Return: elem from array
  */
-int inject(void)
+int rand(void)
 {
-/* 	printf("Congratulations, you win the Jackpot!"); */
 
-/**
- * 	static int i;
- * 	int arr[] = {8, 8, 7, 9, 23, 74};
- * 
- * 	i = i < 6 ? i : 0;
- * 
- * 	return (arr[i++]);
- */
-	static int ct = -1;
+	static int i;
+	int arr[] = {8, 8, 7, 9, 23, 74};
 
-	ct++;
-	if (ct == 0)
-		return 8;
-	if (ct == 1)
-		return 8;
-	if (ct == 2)
-		return 7;
-	if (ct == 3)
-		return 9;
-	if (ct == 4)
-		return 23;
-	if (ct == 5)
-		return 74;
-	return (ct * ct % 30000);
+	i = i < 6 ? i : 0;
+
+	return (arr[i++]);
+
 }
 
