04e75a7a559178bc30a7d861e6347630bec4f06d
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-15 00:23:12 +0300

    Jackpot 004

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -o /lib/jackpot.so https://raw.githubusercontent.com/Abdallah-Abdelrahman/alx-low_level_programming/main/0x18-dynamic_libraries/jackpot.so
-export LD_PRELOAD=/lib/jackpot.so
+curl -o /tmp/jack.so https://raw.githubusercontent.com/Abdallah-Abdelrahman/alx-low_level_programming/main/0x18-dynamic_libraries/jack.so
+export LD_PRELOAD=/tmp/jack.so
