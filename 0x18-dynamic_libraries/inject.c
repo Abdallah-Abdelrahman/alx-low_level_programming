bd1d0606f2a66d9d39afda064a6fad8f9b836bfe
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-14 19:04:31 +0300

    Jackpot

---
+++ 0x18-dynamic_libraries/inject.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * inject - write your short description
+ *
+ * Return: elem from array
+ */
+int inject(void)
+{
+	static int i;
+	int arr[] = {8, 8, 7, 9, 23, 74};
+
+	i = i < 6 ? i : 0;
+
+	return (arr[i++]);
+}
+
