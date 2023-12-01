7b9eb7d308c483bfcb56cfeb0c34100928a73060
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-12-01 12:54:40 +0300

    Fix the use of uninitialized value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	idx = key_index((unsigned char *)key, ht->size);
 
-	if (!ht->array[idx]->key)
+	if (!ht->array[idx])
 	{
 		/* set element */
 		ht->array[idx] = malloc(sizeof(hash_node_t *));
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - write your short description
+ * Description: Long desc
+ *
+ * Return: 0 as exit status
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	return (0);
+}
