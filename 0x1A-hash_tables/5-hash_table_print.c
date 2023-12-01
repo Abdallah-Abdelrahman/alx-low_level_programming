c72e5c2c0634ad8308edca6d7c40c865ae095544
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-12-01 13:39:06 +0300

    Print hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,5 +7,21 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	uli idx, j;
 
+	if (!ht || !ht->array)
+		return;
+
+	printf("{");
+	for (idx = 0, j = 0; idx < ht->size; idx++)
+	{
+		if (ht->array[idx])
+		{
+			if (j)
+				printf(", ");
+			printf("'%s': '%s'", ht->array[idx]->key, ht->array[idx]->value);
+			j++;
+		}
+	}
+	printf("}\n");
 }
