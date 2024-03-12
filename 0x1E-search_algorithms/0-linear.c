a54f532668f024c6566509d8cf37f08a9977b968
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-12 04:35:41 +0300

    Remove some printing

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -22,12 +22,8 @@ int linear_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-		{
-			printf("Found %d at index: %ld\n", value, i);
 			return (i);
-		}
 	}
-	printf("Found %d at index: -1", value);
 
 	return (-1);
 }
