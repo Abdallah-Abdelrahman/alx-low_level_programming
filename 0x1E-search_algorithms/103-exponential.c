d80ef3fe9502d02ce94261c9839797a139dac38f
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-13 06:27:23 +0300

    Exponential search

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -13,5 +13,40 @@
  */
 int exponential_search(int *array, size_t size, int value)
 {
-	return (0);
+	size_t i = 0, start, end, mid;
+
+	if (!array || size == 0)
+		return (-1);
+	if (array[0] == value)
+		return (i);
+
+	start = 0;
+	end = size - 1;
+	for (i = 1; i < size && array[i] <= value; i *= 2)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			i / 2, MIN(i, size - 1));
+	while (start <= end)
+	{
+		mid = start + (end - start) / 2;
+		if (array[mid] < value)
+		{
+			start = mid + 1;
+			PRINT(array, start, end);
+		}
+		else if (array[mid] > value)
+		{
+			end = mid - 1;
+			PRINT(array, start, end);
+		}
+		else
+			return (mid);
+	}
+
+	return (-1);
 }
