fff864e36fbe5497d1d841cde441f3e10894d10c
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-13 05:45:06 +0300

    Update 000-102

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -38,10 +38,14 @@ int interpolation_search(int *array, size_t size, int value)
 		}
 		if (array[pos] > value)
 		{
+			printf("Value checked array[%d] = [%d]\n", pos, array[pos]);
 			hi = pos - 1;
 		}
 		else
+		{
+			printf("Value checked array[%d] = [%d]\n", pos, array[pos]);
 			lo = pos + 1;
+		}
 	}
 
 	printf("Value checked array[%d] is out of range\n",
---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,17 @@
+#include "search_algos.h"
+
+/**
+ * exponential_search - searches for a value in a sorted array of integers.
+ * Description: using the Exponential search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: value to search for
+ *
+ * Return: first index where value is located,
+ * If value is not present in array or if array is NULL,
+ * your function must return -1
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	return (0);
+}
